a99d2929a78a7f08bffef8bd930a49f569d0fcad
Matthew Hunter Steffner <huntersteffner@gmail.com>
2023-10-11 14:19:55 -0400

    Controller update

--- src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GlobalSuperstoreController {
     @GetMapping("/")
     public String getForm(Model model) {
+        model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
 }
