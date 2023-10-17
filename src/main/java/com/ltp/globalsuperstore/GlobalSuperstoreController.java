159123f44586c5aaf46f7baa65b04244667269bd
Matthew Hunter Steffner <huntersteffner@gmail.com>
2023-10-17 15:29:14 -0400

    Started form submission

--- src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
@@ -3,17 +3,30 @@ package com.ltp.globalsuperstore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GlobalSuperstoreController {
+
+    private List<Item> items = new ArrayList<>();
     @GetMapping("/")
     public String getForm(Model model) {
         model.addAttribute("categories", Constants.CATEGORIES);
+        model.addAttribute("item", new Item());
         return "form";
     }
 
     @GetMapping("/inventory")
     public String getInventory() {
         return "inventory";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item) {
+        items.add(item);
+        return "redirect:/inventory";
     }
 }
