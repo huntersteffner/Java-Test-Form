0a935ee1c0b1f9ae7291bdff13e6823b4c55b1b0
Matthew Hunter Steffner <huntersteffner@gmail.com>
2023-10-25 15:23:07 -0400

    Form updates

--- src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
@@ -5,9 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class GlobalSuperstoreController {
@@ -28,10 +31,25 @@ public class GlobalSuperstoreController {
     }
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item) {
-        items.add(item);
+    public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
+        int index = getIndexFromId(item.getId());
+        String status = Constants.SUCCESS_STATUS;
+        if(index == Constants.NOT_Found) {
+            items.add(item);
+        } else if(within5Days(item.getDate(),items.get(index).getDate())) {
+            items.set(index, item);
+        } else {
+            status = Constants.FAILED_STATUS;
+        }
+        redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/inventory";
     }
+
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
+    }
+
 
     public int getIndexFromId(String id) {
         for (int i = 0; i < items.size(); i++) {
